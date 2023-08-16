e8676873b235be4a31ccd141c772880a6fa485a4
chashu <chashu@chashus-MacBook-Pro.local>
2023-08-17 00:49:20 +0900

    本番運用

--- api_playn/settings.py
+++ api_playn/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 AUTH_USER_MODEL = "apiapp.User"
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "media/"
