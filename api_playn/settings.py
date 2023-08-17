60c2542964a3cf3147c81ec2094e4b4a3e8e1b1a
chashu <chashu@chashus-MacBook-Pro.local>
2023-08-17 10:34:51 +0900

    本番運用

--- api_playn/settings.py
+++ api_playn/settings.py
@@ -8,11 +8,12 @@ SECRET_KEY = 'django-insecure-q0(15pxso)=(1+kt=(ns=1qh=^i6tb@*dz_e5(l)fh04mh0b80
 
 DEBUG = True
 
+# ALLOWED_HOSTS = [
+#     '.vercel.app',
+#     'localhost',
+#     ]
 ALLOWED_HOSTS = [
-    '.vercel.app',
-    'localhost',
-    ".now.sh",
-    "*"
+    "*",
     ]
 
 
@@ -118,8 +119,9 @@ USE_TZ = True
 AUTH_USER_MODEL = "apiapp.User"
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "media/"
